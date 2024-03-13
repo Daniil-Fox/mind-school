a5e1eb754d6edc084a9239741fa66ad6142aaef6
Artyushenko_Daniil <foxones80@gmail.com>
2024-03-13 16:47:10 +0600

    fixed some

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,7 +1,18 @@
 
 import Swiper from 'swiper';
-
+import { Navigation, Pagination } from 'swiper/modules';
 const swiper = new Swiper('.coachers__slider', {
   slidesPerView: 3,
   spaceBetween: 30
+});
+
+new Swiper('.testimonials__slider', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  loop: true,
+
+  navigation : {
+    prevEl: '.testimonials__btn--prev',
+    nextEl: '.testimonials__btn--next',
+  }
 });
